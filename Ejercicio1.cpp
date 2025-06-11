a07c04212fa43f710e3a0383eb8a3d96e985cbf5
rodrigoLLC <rllanoc@unjbg.edu.pe>
2025-06-11 13:29:24 -0500

    Ejercicio1

---
+++ Ejercicio1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+    int n, max, A[20];
+
+    cin >> n;
+
+    for ( int i = 0; i < n; i++) {
+        cin >> A[i];
+
+    }
+    max = A[0];
+
+    for ( int i = 1; i < n ; i++ ) {
+        if ( A[i] > max ) {
+            max = A[i];
+        }
+    }
+    cout << max;
+
+    return 0;
+
+}
